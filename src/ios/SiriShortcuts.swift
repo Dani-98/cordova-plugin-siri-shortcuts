c750cfae82b1c62b3e8b409475ae68ddd3d6d042
fadelatorre <fadelatorre@intermarkit.es>
2019-12-10 13:41:44 +0100

    Revert "Se añaden ficheros para custom intent"
    This reverts commit 42c44e88a5238ff7ae7ca4a3e83f87c9d4273a25.

--- src/ios/SiriShortcuts.swift
+++ src/ios/SiriShortcuts.swift
@@ -15,38 +15,19 @@ import IntentsUI
     }
 
     @objc(donate:) func donate(_ command: CDVInvokedUrlCommand) {
-        // self.commandDelegate!.run(inBackground: {
-        //     if #available(iOS 12.0, *) {
-        //         self.activity = self.createUserActivity(from: command, makeActive: true)
+        self.commandDelegate!.run(inBackground: {
+            if #available(iOS 12.0, *) {
+                self.activity = self.createUserActivity(from: command, makeActive: true)
 
-        //         // tell Cordova we're all OK
-        //         self.sendStatusOk(command)
+                // tell Cordova we're all OK
+                self.sendStatusOk(command)
 
-        //         return
-        //     }
-
-        //     // shortcut not donated
-        //     self.sendStatusError(command)
-        // })
-
-        let intent = PersonInfoIntent()
-        intent.suggestedInvocationPhrase = "Add person Info"
-        intent.firstName = "firstName"
-        intent.lastName = "lastName"
-        intent.companyName = "companyName"
-        let interaction = INInteraction(intent: intent, response: nil)
-        
-        interaction.donate { (error) in
-            if error != nil {
-                if let error = error as NSError? {
-                    print("Interaction donation failed: \(error.description)")
-                } else {
-                    print("Successfully donated interaction")
-                }
+                return
             }
-        }
 
-        self.sendStatusOk(command)
+            // shortcut not donated
+            self.sendStatusError(command)
+        })
     }
 
     @objc(present:) func present(_ command: CDVInvokedUrlCommand) {
